61b5eb706af42ce02f8d15f73678cbf6bfa90bf7
Gabriela Almeida <45702136+connwcted@users.noreply.github.com>
2024-06-11 23:15:41 +0100

    Background

---
+++ Assets/Scripts/MoveBG.cs
@@ -0,0 +1,21 @@
+using Melanchall.DryWetMidi.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBG : MonoBehaviour
+{
+    public float speed = 10f;
+    public Renderer bgRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+    }
+}
