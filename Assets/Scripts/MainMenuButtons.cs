7ded904b4dccc087067c76fc9a8d16ea3023ea18
Gabriela Almeida <45702136+connwcted@users.noreply.github.com>
2024-06-12 13:36:30 +0100

    Main Menu com botões

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("FrostLand");
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+
+}
