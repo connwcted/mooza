9795249ba81745d60ec4194c002e45f26252f012
Gabriela Almeida <45702136+connwcted@users.noreply.github.com>
2024-06-14 16:36:51 +0100

    Level Vinxis

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -9,6 +9,7 @@ public class LevelSelect : MonoBehaviour
 {
     public Button FrostLandButton;
     public Button PhotoButton;
+    public Button VinxisButton;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +33,11 @@ public class LevelSelect : MonoBehaviour
         PhotoButton.onClick.AddListener(() =>
         {
             SceneManager.LoadScene("Photo");
+        });
+
+        VinxisButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Vinxis");
         });
     }
 }
